440adf7c9960c6b95f9be73f9014d79ded5c34ae
LashinCHE <egor.cherevichin1337@gmail.com>
2024-05-12 16:47:26 +0300

    finish

--- variant.py
+++ variant.py
@@ -1,8 +1,8 @@
 from pygost import gost34112012256
 
 try:
-    data = "Черевичин Егор Викторович"  # Unicode string
-    encoded_data = data.encode("utf-8")  # Encode the Unicode string to bytes
+    data = "Черевичин Егор Викторович"  
+    encoded_data = data.encode("utf-8")  
     hash_object = gost34112012256.new(encoded_data)
     hash_result = hash_object.digest()
     print(hash_result)
